fbcaf67b66b4c2e01a091bdad9143b4a5a84991b
Alfred Wong <alfred.cl.wong@gmail.com>
2021-03-17 20:51:20 +0000

    JuliaLang testset rewrite

--- test/testsets/JuliaLang.jl
+++ test/testsets/JuliaLang.jl
@@ -12,61 +12,36 @@ and pop it from the set
 =#
 
 @otestset "JuliaLang" begin
-    # ensure there aren't any ambiguities or unbound args
-    let unbound = Set{Method}(detect_unbound_args(OpticSim))
-        # Pretty hacky way to ignore specific methods, for some weird reason the unbound args check seems to fail for some (seemingly random) methods with Vararg arguments
-        # here we ignore the specific methods which we know are ok but are still failing
-        if VERSION >= v"1.6.0-DEV"
-            pop!(unbound, which(OpticSim.LensAssembly, Tuple{Vararg{Union{CSGTree{T},LensAssembly{T},Surface{T}},N} where N} where {T<:Real}))
-            pop!(unbound, which(OpticSim.RayListSource, Tuple{Vararg{OpticalRay{T,3},N} where N} where {T<:Real}))
-            pop!(unbound, which(OpticSim.OpticalSourceGroup, Tuple{Vararg{OpticalRayGenerator{T},N} where N} where {T<:Real}))
-            pop!(unbound, which(OpticSim.PixelSource, Tuple{Vararg{P,N} where N} where {P<:OpticalRayGenerator{T}} where {T<:Real}))
-            pop!(unbound, which(OpticSim.MultiHologramInterface, Tuple{Vararg{HologramInterface{T},N}} where {N} where {T<:Real}))
-        end
-        # and ignore any generate methods created due to default or keyword arguments
-        for m in unbound
-            if occursin("#", string(m.name))
-                pop!(unbound, m)
-            end
-        end
-        @test unbound == Set()
-    end
-    let ambiguous = Set{Method}(detect_ambiguities(OpticSim))
-        @test ambiguous == Set()
-    end
-    let unbound = Set{Method}(detect_unbound_args(OpticSim.Zernike))
-        @test unbound == Set()
-    end
-    let ambiguous = Set{Method}(detect_ambiguities(OpticSim.Zernike))
-        @test ambiguous == Set()
-    end
-    let unbound = Set{Method}(detect_unbound_args(OpticSim.QType))
-        @test unbound == Set()
-    end
-    let ambiguous = Set{Method}(detect_ambiguities(OpticSim.QType))
-        @test ambiguous == Set()
-    end
-    let unbound = Set{Method}(detect_unbound_args(OpticSim.Vis))
-        # Pretty hacky way to ignore specific methods, for some weird reason the unbound args check seems to fail for some (seemingly random) methods with Vararg arguments
-        # here we ignore the specific methods which we know are ok but are still failing
-        pop!(unbound, which(OpticSim.Vis.drawcurves, Tuple{Vararg{Spline{P,S,N,M},N1} where N1} where {M} where {N} where {S} where {P}))
-        pop!(unbound, which(OpticSim.Vis.draw, Tuple{Vararg{S,N} where N} where {S<:Union{OpticSim.Surface{T},OpticSim.TriangleMesh{T}}} where {T<:Real}))
-        pop!(unbound, which(OpticSim.Vis.draw!, Tuple{OpticSim.Vis.MakieLayout.LScene,Vararg{S,N} where N} where {S<:Union{OpticSim.Surface{T},OpticSim.TriangleMesh{T}}} where {T<:Real}))
-        # and ignore any generate methods created due to default or keyword arguments
-        for m in unbound
-            if occursin("#", string(m.name))
-                pop!(unbound, m)
-            end
-        end
-        @test unbound == Set()
-    end
-    let ambiguous = Set{Method}(detect_ambiguities(OpticSim.Vis))
-        @test ambiguous == Set()
-    end
-    let unbound = Set{Method}(detect_unbound_args(OpticSim.TestData))
-        @test unbound == Set()
-    end
-    let ambiguous = Set{Method}(detect_ambiguities(OpticSim.TestData))
-        @test ambiguous == Set()
+    # here we ignore the specific methods which we know are ok but are still failing
+    # for some weird reason the unbound args check seems to fail for some (seemingly random) methods with Vararg arguments
+    methods_to_ignore = Dict(
+        OpticSim => VERSION >= v"1.6.0-DEV" ? [
+            (OpticSim.LensAssembly, Tuple{Vararg{Union{CSGTree{T},LensAssembly{T},Surface{T}},N} where N} where {T<:Real}),
+            (OpticSim.RayListSource, Tuple{Vararg{OpticalRay{T,3},N} where N} where {T<:Real}),
+            (OpticSim.OpticalSourceGroup, Tuple{Vararg{OpticalRayGenerator{T},N} where N} where {T<:Real}),
+            (OpticSim.PixelSource, Tuple{Vararg{P,N} where N} where {P<:OpticalRayGenerator{T}} where {T<:Real}),
+            (OpticSim.MultiHologramInterface, Tuple{Vararg{HologramInterface{T},N}} where {N} where {T<:Real}),
+        ] : [],
+        OpticSim.Zernike => [],
+        OpticSim.QType => [],
+        OpticSim.Vis => [
+            (OpticSim.Vis.drawcurves, Tuple{Vararg{Spline{P,S,N,M},N1} where N1} where {M} where {N} where {S} where {P}),
+            (OpticSim.Vis.draw, Tuple{Vararg{S,N} where N} where {S<:Union{OpticSim.Surface{T},OpticSim.TriangleMesh{T}}} where {T<:Real}),
+            (OpticSim.Vis.draw!, Tuple{OpticSim.Vis.MakieLayout.LScene,Vararg{S,N} where N} where {S<:Union{OpticSim.Surface{T},OpticSim.TriangleMesh{T}}} where {T<:Real})
+        ],
+        OpticSim.TestData => [],
+    )
+
+    for (mod, ignore_list) in methods_to_ignore
+        unbound = setdiff(
+            detect_unbound_args(mod),
+            [which(method, signature) for (method, signature) in ignore_list]
+        )
+        # also ignore any generate methods created due to default or keyword arguments
+        filter!(m -> !occursin("#", string(m.name)), unbound)
+        @test isempty(unbound)
+
+        ambiguous = detect_ambiguities(mod)
+        @test isempty(ambiguous)
     end
 end # testset JuliaLang
