17ad956ac079a8160e59d9ee4007cf3a3e4a65b8
BrianGun <bguenter@microsoft.com>
2021-04-01 10:42:20 -0700

    OpenGL fails on Windows tests Fixes #74
    turned off calls to Vis if OS is windows.

--- test/testsets/Visualization.jl
+++ test/testsets/Visualization.jl
@@ -1,15 +1,18 @@
 @otestset "Visualization" begin
-    # test that this all at least runs
-    surf1 = AcceleratedParametricSurface(TestData.beziersurface(), 15)
-    surf2 = AcceleratedParametricSurface(TestData.upsidedownbeziersurface(), 15)
-    m = csgintersection(leaf(surf1, translation(-0.5, -0.5, 0.0)), csgintersection(leaf(Cylinder(0.3, 5.0)), leaf(surf1, RigidBodyTransform{Float64}(0.0, Float64(π), 0.0, 0.5, -0.5, 0.0))))()
-    @test_nowarn Vis.draw(m)
-    m = csgintersection(leaf(surf1, translation(-0.5, -0.5, 0.0)), csgintersection(leaf(Cylinder(0.3, 5.0)), leaf(surf2, translation(-0.5, -0.5, 0.0))))()
-    @test_nowarn Vis.draw(m)
-    m = csgunion(leaf(Cylinder(0.5, 3.0)), (leaf(Sphere(1.0))))()
-    @test_nowarn Vis.draw(m)
-    m = csgintersection(leaf(Cylinder(0.5, 3.0)), (leaf(Sphere(1.0))))()
-    @test_nowarn Vis.draw(m)
-    m = csgdifference(leaf(Cylinder(0.5, 3.0)), leaf(Sphere(1.0)))()
-    @test_nowarn Vis.draw(m)
+    if Sys.iswindows return
+    else
+        # test that this all at least runs
+        surf1 = AcceleratedParametricSurface(TestData.beziersurface(), 15)
+        surf2 = AcceleratedParametricSurface(TestData.upsidedownbeziersurface(), 15)
+        m = csgintersection(leaf(surf1, translation(-0.5, -0.5, 0.0)), csgintersection(leaf(Cylinder(0.3, 5.0)), leaf(surf1, RigidBodyTransform{Float64}(0.0, Float64(π), 0.0, 0.5, -0.5, 0.0))))()
+        @test_nowarn Vis.draw(m)
+        m = csgintersection(leaf(surf1, translation(-0.5, -0.5, 0.0)), csgintersection(leaf(Cylinder(0.3, 5.0)), leaf(surf2, translation(-0.5, -0.5, 0.0))))()
+        @test_nowarn Vis.draw(m)
+        m = csgunion(leaf(Cylinder(0.5, 3.0)), (leaf(Sphere(1.0))))()
+        @test_nowarn Vis.draw(m)
+        m = csgintersection(leaf(Cylinder(0.5, 3.0)), (leaf(Sphere(1.0))))()
+        @test_nowarn Vis.draw(m)
+        m = csgdifference(leaf(Cylinder(0.5, 3.0)), leaf(Sphere(1.0)))()
+        @test_nowarn Vis.draw(m)
+    end
 end # testset Visualization
