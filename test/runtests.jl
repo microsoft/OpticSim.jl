5f290a26e4dd3ffc6c38937680babc3ea8ddaab2
Alfred Wong <alfred.cl.wong@gmail.com>
2021-04-15 11:22:39 +0100

    Test -> XUnit

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE
 
-using Test
+using XUnit
 using FiniteDifferences
 using StaticArrays
 using LinearAlgebra
