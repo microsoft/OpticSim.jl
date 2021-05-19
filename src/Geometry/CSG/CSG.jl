21c215d352a6d7863ec4347c734e92a29c728273
Alfred Wong <alfred.cl.wong@gmail.com>
2021-05-19 20:56:05 +0100

    Revert "CSG2 suggestion"
    This reverts commit 515025e1b5b7f6a235f6c2679895d02fe005d336.

--- src/Geometry/CSG/CSG.jl
+++ src/Geometry/CSG/CSG.jl
@@ -2,7 +2,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # See LICENSE in the project root for full license information.
 
-include("CSG2.jl")
 import Base: ∩, ∪, -
 
 export CSGTree, CSGGenerator, leaf
