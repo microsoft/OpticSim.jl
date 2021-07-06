d5e71461be849fe98573274765efbeed414343b6
BrianGun <bguenter@microsoft.com>
2021-07-06 16:44:18 -0700

    added license line

--- src/Examples/ExamplesRepeatingStructures.jl
+++ src/Examples/ExamplesRepeatingStructures.jl
@@ -1,3 +1,11 @@
+# MIT license
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# See LICENSE in the project root for full license information.
+
+
+
+#############################################################################
+
 drawneighbors() = Vis.@wrapluxor Vis.drawhexcells(50,Repeat.neighbors(Repeat.HexBasis1,(0,0),2))
 export drawneighbors
 
