a7a627181ba921a841a15e202ea48dc9379a7f35
BrianGun <bguenter@microsoft.com>
2021-07-25 13:19:18 -0700

    minor change to LensletAssembly

--- src/Optical/ParaxialAnalysis/LensletAssembly.jl
+++ src/Optical/ParaxialAnalysis/LensletAssembly.jl
@@ -1,10 +1,10 @@
 """Encapsulates the lens assembly and the display for one lenslet."""
 struct LensletAssembly{T}
-    lens::LensAssembly
+    lens #need to figure out how to type this
     transform::Geometry.Transform
     display #composite emitter array maybe? See if emitter arrays are greedily or lazily constructed.
 end
 
-lens(a::LensLetAssembly) = a.lens
-display(a::LensLetAssembly) = a.display
-transform(a::LensLetAssembly) = a.transform
+lens(a::LensletAssembly) = a.lens
+display(a::LensletAssembly) = a.display
+transform(a::LensletAssembly) = a.transform
