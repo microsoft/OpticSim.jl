7784b975defe4935b0335050c3dd096a8dabdf40
BrianGun <bguenter@microsoft.com>
2021-08-05 16:03:51 -0700

    Tessellation tools: clusters Fixes #257 added cluster lattice modified latticebasis to use matrix to represent basis vectors instead of SVector{SVector}}. Simpler.

---
+++ src/RepeatingStructures/Cluster.jl
@@ -0,0 +1,26 @@
+"""A Cluster is a repeating pattern of indices defined in a lattice called the element lattice. The cluster of elements has its own lattice, which by definition is different from the element lattice unless each cluster consists of a single element. For example this defines a Cluster of three hexagonal elements:
+
+Cluster()"""
+struct Cluster{N1, B1{N,T<:Real}<:Basis{N,T},B2{N,T<:Real}<:Basis{N,T}}
+    clusterbasis::B1{N,T}
+    elementbasis::B2{N,T}
+
+    clusterelements::SVector{N1,NTuple{N,Int}} #vector containing the lattice indices of the elements in the cluster. Each column is one lattice coordinate. These indices are assumed to be offsets from the origin of the lattice.
+end
+
+"""returns the positions of every element in a cluster given the cluster indices"""
+function Base.getindex(A::Cluster{N1,B1{N,T},B2{N,T}}, indices::Vararg{Int, N}) where{N} 
+    clusteroffset = A.clusterbasis[indices]
+    temp = MMatrix{N,N1,T}(undef)
+    for i in 1:N1
+        temp[:,i] = A.elementbasis[A.clusterelements[i]] + clusteroffset
+    end
+    return SMatrix{N,N1,T}(temp) #positions of the cluster elements, not the lattice coordinates.
+end
+
+"""Can access any cluster in a cluster lattice so the range of indices is unlimited"""
+function Base.size(a::Cluster{N,B1{N,T},B2{N,T}}) where{N,T}
+    return ntuple((i)->Base.IsInfinite(),N)
+end
+
+Base.setindex!(A::Basis{N,T}, v, I::Vararg{Int, N}) where{T,N} = nothing #can't set lattice points. Might want to throw an exception instead.
