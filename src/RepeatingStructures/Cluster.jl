f2bb9fc187c2f1d0d80d781433486dad0729ed2e
BrianGun <bguenter@microsoft.com>
2021-08-05 16:18:39 -0700

    started working on lattice clusters

--- src/RepeatingStructures/Cluster.jl
+++ src/RepeatingStructures/Cluster.jl
@@ -1,15 +1,15 @@
 """A Cluster is a repeating pattern of indices defined in a lattice called the element lattice. The cluster of elements has its own lattice, which by definition is different from the element lattice unless each cluster consists of a single element. For example this defines a Cluster of three hexagonal elements:
 
 Cluster()"""
-struct Cluster{N1, B1{N,T<:Real}<:Basis{N,T},B2{N,T<:Real}<:Basis{N,T}}
-    clusterbasis::B1{N,T}
-    elementbasis::B2{N,T}
+struct LatticeCluster{N1, B1<:Basis{N,T},B2<:Basis{N,T}}
+    clusterbasis::B1
+    elementbasis::B2
 
     clusterelements::SVector{N1,NTuple{N,Int}} #vector containing the lattice indices of the elements in the cluster. Each column is one lattice coordinate. These indices are assumed to be offsets from the origin of the lattice.
 end
 
 """returns the positions of every element in a cluster given the cluster indices"""
-function Base.getindex(A::Cluster{N1,B1{N,T},B2{N,T}}, indices::Vararg{Int, N}) where{N} 
+function Base.getindex(A::LatticeCluster{N1,B1{N,T},B2{N,T}}, indices::Vararg{Int, N}) where{N} 
     clusteroffset = A.clusterbasis[indices]
     temp = MMatrix{N,N1,T}(undef)
     for i in 1:N1
@@ -19,8 +19,17 @@ function Base.getindex(A::Cluster{N1,B1{N,T},B2{N,T}}, indices::Vararg{Int, N}) 
 end
 
 """Can access any cluster in a cluster lattice so the range of indices is unlimited"""
-function Base.size(a::Cluster{N,B1{N,T},B2{N,T}}) where{N,T}
+function Base.size(::LatticeCluster{N,B1{N,T},B2{N,T}}) where{N,T}
     return ntuple((i)->Base.IsInfinite(),N)
 end
 
 Base.setindex!(A::Basis{N,T}, v, I::Vararg{Int, N}) where{T,N} = nothing #can't set lattice points. Might want to throw an exception instead.
+
+"""
+Example:
+lenslets = DataFrame()
+"""
+struct LensletCluster
+    lattice::LatticeCluster
+    properties::DataFrame
+end
