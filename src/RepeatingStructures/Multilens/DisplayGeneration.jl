a47db4225e6bc2f453933adecc4a598a78e253a3
BrianGun <bguenter@microsoft.com>
2021-11-21 15:59:30 -0800

    added license to DisplayGeneration.jl

--- src/RepeatingStructures/Multilens/DisplayGeneration.jl
+++ src/RepeatingStructures/Multilens/DisplayGeneration.jl
@@ -1,3 +1,8 @@
+# MIT license
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# See LICENSE in the project root for full license information.
+
+
 #display lenslet systems: emitter, paraxial lenslet, paraxial eye lens, retina detector.
 #start with eyebox plane and hexagonal tiling. Project onto display surface, generate lenslets automatically.
 
