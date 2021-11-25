534ef1cff53b48ad6dff19d37dc376fe59bfafc4
BrianGun <bguenter@microsoft.com>
2021-11-25 11:28:25 -0800

    undoing PlanarShape -> AbstractPlanarShape change. This should be a separate PR.

--- src/Geometry/Primitives/NonCSG/ConvexPolygon.jl
+++ src/Geometry/Primitives/NonCSG/ConvexPolygon.jl
@@ -7,7 +7,7 @@ import Statistics
 const _abs_err_orientation_2d = 2*eps(Float64)
 
 """
-    ConvexPolygon{N, T<:Real} <: AbstractPlanarShape{T} 
+    ConvexPolygon{N, T<:Real} <: PlanarShape{T} 
 
 General Convex Polygon surface, not a valid CSG object.
 The rotation of the polygon around its normal is defined by `rotationvec`.
@@ -21,7 +21,7 @@ The local frame defines the plane (spans by the right and up vectors) with the p
 the local_polygon_points are given with respect to the local frame and are 2D points.
 NOTE: This class uses static vectors to hold the points which will lead to more efficient performance, but should not be used with polygons with more than 20-30 points.
 """
-struct ConvexPolygon{N,T<:Real}  <: AbstractPlanarShape{T} 
+struct ConvexPolygon{N,T<:Real}  <: PlanarShape{T} 
     plane::Plane{T,3}
     local_frame::Transform{T}
     # local_points::Vector{SVector{2, T}}
