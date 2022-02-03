46869ebb87bac1b7e5a6fd9c13e11d714a00bad2
BrianGun <bguenter@microsoft.com>
2022-02-02 19:37:44 -0800

    systems.jl in test suite has almost, but not quite, exact duplicate of otherexamples.jl. Which had the old SCHOTT glasses. Fixed to use hardcoded glasses.

--- test/TestData/systems.jl
+++ test/TestData/systems.jl
@@ -2,13 +2,23 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # See LICENSE in the project root for full license information.
 
+const Examples_N_BK7 = GlassCat.Glass(GlassCat.GlassID(GlassCat.AGF, 885), 2, 1.03961212, 0.00600069867, 0.231792344, 0.0200179144, 1.01046945, 103.560653, 0.0, 0.0, NaN, NaN, 0.3, 2.5, 1.86e-6, 1.31e-8, -1.37e-11, 4.34e-7, 6.27e-10, 0.17, 20.0, -0.0009, 2.3, 1.0, 7.1, 1.0, 1, 1.0, [(0.3, 0.05, 25.0), (0.31, 0.25, 25.0), (0.32, 0.52, 25.0), (0.334, 0.78, 25.0), (0.35, 0.92, 25.0), (0.365, 0.971, 25.0), (0.37, 0.977, 25.0), (0.38, 0.983, 25.0), (0.39, 0.989, 25.0), (0.4, 0.992, 25.0), (0.405, 0.993, 25.0), (0.42, 0.993, 25.0), (0.436, 0.992, 25.0), (0.46, 0.993, 25.0), (0.5, 0.994, 25.0), (0.546, 0.996, 25.0), (0.58, 0.995, 25.0), (0.62, 0.994, 25.0), (0.66, 0.994, 25.0), (0.7, 0.996, 25.0), (1.06, 0.997, 25.0), (1.53, 0.98, 25.0), (1.97, 0.84, 25.0), (2.325, 0.56, 25.0), (2.5, 0.36, 25.0)], 1.5168, 2.3, 0.0, 0, 64.17, 0, 2.51, 0.0)
+
+const Examples_N_SK16 =GlassCat.Glass(GlassCat.GlassID(GlassCat.AGF, 828), 2, 1.34317774, 0.00704687339, 0.241144399, 0.0229005, 0.994317969, 92.7508526, 0.0, 0.0, NaN, NaN, 0.31, 2.5, -2.37e-8, 1.32e-8, -1.29e-11, 4.09e-7, 5.17e-10, 0.17, 20.0, -0.0011, 3.2, 1.4, 6.3, 4.0, 1, 53.3, [(0.31, 0.02, 25.0), (0.32, 0.11, 25.0), (0.334, 0.4, 25.0), (0.35, 0.7, 25.0), (0.365, 0.86, 25.0), (0.37, 0.89, 25.0), (0.38, 0.93, 25.0), (0.39, 0.956, 25.0), (0.4, 0.97, 25.0), (0.405, 0.974, 25.0), (0.42, 0.979, 25.0), (0.436, 0.981, 25.0), (0.46, 0.984, 25.0), (0.5, 0.991, 25.0), (0.546, 0.994, 25.0), (0.58, 0.994, 25.0), (0.62, 0.993, 25.0), (0.66, 0.994, 25.0), (0.7, 0.996, 25.0), (1.06, 0.995, 25.0), (1.53, 0.973, 25.0), (1.97, 0.88, 25.0), (2.325, 0.54, 25.0), (2.5, 0.26, 25.0)], 1.62041, 3.3, 4.0, 0, 60.32, 0, 3.58, 0.0)
+
+
+const Examples_N_SF2 = GlassCat.Glass(GlassCat.GlassID(GlassCat.AGF, 815), 2, 1.47343127, 0.0109019098, 0.163681849, 0.0585683687, 1.36920899, 127.404933, 0.0, 0.0, NaN, NaN, 0.365, 2.5, 3.1e-6, 1.75e-8, 6.62e-11, 7.51e-7, 8.99e-10, 0.277, 20.0, 0.0081, 1.0, 1.4, 6.68, 1.0, 1, 1.0, [(0.365, 0.007, 25.0), (0.37, 0.06, 25.0), (0.38, 0.4, 25.0), (0.39, 0.68, 25.0), (0.4, 0.83, 25.0), (0.405, 0.865, 25.0), (0.42, 0.926, 25.0), (0.436, 0.949, 25.0), (0.46, 0.961, 25.0), (0.5, 0.975, 25.0), (0.546, 0.986, 25.0), (0.58, 0.987, 25.0), (0.62, 0.984, 25.0), (0.66, 0.984, 25.0), (0.7, 0.987, 25.0), (1.06, 0.997, 25.0), (1.53, 0.984, 25.0), (1.97, 0.93, 25.0), (2.325, 0.76, 25.0), (2.5, 0.67, 25.0)], 1.64769, 1.2, 0.0, 0, 33.82, 0, 2.718, 0.0)
+
+const Examples_N_SF14 = GlassCat.Glass(GlassCat.GlassID(GlassCat.AGF, 896), 2, 1.69022361, 0.0130512113, 0.288870052, 0.061369188, 1.7045187, 149.517689, 0.0, 0.0, NaN, NaN, 0.365, 2.5, -5.56e-6, 7.09e-9, -1.09e-11, 9.85e-7, 1.39e-9, 0.287, 20.0, 0.013, 1.0, 2.2, 9.41, 1.0, 1, 1.0, [(0.365, 0.004, 25.0), (0.37, 0.04, 25.0), (0.38, 0.33, 25.0), (0.39, 0.61, 25.0), (0.4, 0.75, 25.0), (0.405, 0.79, 25.0), (0.42, 0.87, 25.0), (0.436, 0.91, 25.0), (0.46, 0.938, 25.0), (0.5, 0.964, 25.0), (0.546, 0.983, 25.0), (0.58, 0.987, 25.0), (0.62, 0.987, 25.0), (0.66, 0.987, 25.0), (0.7, 0.985, 25.0), (1.06, 0.998, 25.0), (1.53, 0.98, 25.0), (1.97, 0.88, 25.0), (2.325, 0.64, 25.0), (2.5, 0.57, 25.0)], 1.76182, 1.0, 0.0, 0, 26.53, 0, 3.118, 0.0)
+
+
 function cooketriplet(::Type{T} = Float64) where {T<:Real}
     return AxisymmetricOpticalSystem{T}(
         DataFrame(
             SurfaceType = ["Object", "Standard", "Standard", "Standard", "Stop", "Standard", "Standard", "Image"],
             Radius = [Inf, 26.777, 66.604, -35.571, 35.571, 35.571, -26.777, Inf],
             Thickness = [Inf, 4.0, 2.0, 4.0, 2.0, 4.0, 44.748, missing],
-            Material = [Air, SCHOTT.N_SK16, Air, SCHOTT.N_SF2, Air, SCHOTT.N_SK16, Air, missing],
+            Material = [Air, Examples_N_SK16, Air, Examples_N_SF2, Air, Examples_N_SK16, Air, missing],
             SemiDiameter = [Inf, 8.580, 7.513, 7.054, 6.033, 7.003, 7.506, 15.0]
         )
     )
@@ -20,7 +30,7 @@ function cooketripletfirstelement(::Type{T} = Float64) where {T<:Real}
             SurfaceType = ["Object", "Standard", "Standard", "Image"],
             Radius = [Inf, -35.571, 35.571, Inf],
             Thickness = [Inf, 4.0, 44.748, missing],
-            Material = [Air, SCHOTT.N_SK16, Air, missing],
+            Material = [Air, Examples_N_SK16, Air, missing],
             SemiDiameter = [Inf, 7.054, 6.033, 15.0]
         )
     )
@@ -32,7 +42,7 @@ function convexplano(::Type{T} = Float64) where {T<:Real}
             SurfaceType = ["Object", "Standard", "Standard", "Image"],
             Radius = [Inf, 60.0, Inf, Inf],
             Thickness = [Inf, 10.0, 57.8, missing],
-            Material = [Air, SCHOTT.N_BK7, Air, missing],
+            Material = [Air, Examples_N_BK7, Air, missing],
             SemiDiameter = [Inf, 9.0, 9.0, 15.0]
         )
     )
@@ -47,7 +57,7 @@ function doubleconvex(
             SurfaceType = ["Object", "Standard", "Standard", "Image"],
             Radius = [T(Inf64), frontradius, rearradius, T(Inf64)],
             Thickness = [T(Inf64), T(10.0), T(57.8), missing],
-            Material = [Air, SCHOTT.N_BK7, Air, missing],
+            Material = [Air, Examples_N_BK7, Air, missing],
             SemiDiameter = [T(Inf64), T(9.0), T(9.0), T(15.0)]
         );
         temperature,
@@ -63,7 +73,7 @@ function doubleconvex(
             SurfaceType = ["Object", "Standard", "Standard", "Image"],
             Radius = [Inf64, 60, -60, Inf64],
             Thickness = [Inf64, 10.0, 57.8, missing],
-            Material = [Air, SCHOTT.N_BK7, Air, missing],
+            Material = [Air, Examples_N_BK7, Air, missing],
             SemiDiameter = [Inf64, 9.0, 9.0, 15.0]
         );
         temperature,
@@ -76,7 +86,7 @@ function doubleconcave(::Type{T} = Float64) where {T<:Real}
         DataFrame(SurfaceType = ["Object", "Standard", "Standard", "Image"],
             Radius = [Inf64, -41.0, 41.0, Inf64],
             Thickness = [Inf64, 10.0, 57.8, missing],
-            Material = [Air, SCHOTT.N_BK7, Air, missing],
+            Material = [Air, Examples_N_BK7, Air, missing],
             SemiDiameter = [Inf64, 9.0, 9.0, 15.0]
         )
     )
@@ -87,7 +97,7 @@ function planoconcaverefl(::Type{T} = Float64) where {T<:Real}
         DataFrame(SurfaceType = ["Object", "Standard", "Standard", "Image"],
             Radius = [Inf64, Inf64, -41.0, Inf64],
             Thickness = [Inf64, 10.0, -57.8, missing],
-            Material = [Air, SCHOTT.N_BK7, Air, missing],
+            Material = [Air, Examples_N_BK7, Air, missing],
             SemiDiameter = [Inf64, 9.0, 9.0, 25.0],
             Reflectance = [missing, missing, 1.0, missing]
         )
@@ -99,7 +109,7 @@ function concaveplano(::Type{T} = Float64) where {T<:Real}
         DataFrame(SurfaceType = ["Object", "Standard", "Standard", "Image"],
             Radius = [Inf64, -41.0, Inf64, Inf64],
             Thickness = [Inf64, 10.0, 57.8, missing],
-            Material = [Air, SCHOTT.N_BK7, Air, missing],
+            Material = [Air, Examples_N_BK7, Air, missing],
             SemiDiameter = [Inf64, 9.0, 9.0, 15.0]
         )
     )
@@ -110,7 +120,7 @@ function planoplano(::Type{T} = Float64) where {T<:Real}
         DataFrame(SurfaceType = ["Object", "Standard", "Standard", "Image"],
             Radius = [Inf64, Inf64, Inf64, Inf64],
             Thickness = [Inf64, 10.0, 57.8, missing],
-            Material = [Air, SCHOTT.N_BK7, Air, missing],
+            Material = [Air, Examples_N_BK7, Air, missing],
             SemiDiameter = [Inf64, 9.0, 9.0, 15.0]
         )
     )
@@ -124,7 +134,7 @@ placed at the end of the stack. This is for the purpose of benchmarking allocati
 shared Vector within LensAssembly.
 """
 function planarshapes()
-    interface = FresnelInterface{Float64}(SCHOTT.N_BK7, Air; interfacemode=Transmit)
+    interface = FresnelInterface{Float64}(Examples_N_BK7, Air; interfacemode=Transmit)
     s = 10.0
     surfacenormal = SVector(0., 0., 1.)
 
