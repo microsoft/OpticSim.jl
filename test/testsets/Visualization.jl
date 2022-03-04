98d901d555a924eb0d988056335545df418ab4c8
BrianGun <bguenter@microsoft.com>
2022-03-04 13:14:19 -0800

    fixed visualization test that was using Examples.hex3RGB(). This function was moved to Repeat.Multilens.

--- test/testsets/Visualization.jl
+++ test/testsets/Visualization.jl
@@ -37,7 +37,7 @@
         m = (Cylinder(0.5, 3.0) - Sphere(1.0))()
         @test_nowarn Vis.draw(m)
 
-        @test_nowarn Vis.draw(Examples.hex3RGB(),[0 1 0;0 0 1])
+        @test_nowarn Vis.draw(Repeat.Multilens.hex3RGB(),[0 1 0;0 0 1])
         @test_nowarn Examples.drawhexrect()
         @test_nowarn Examples.drawhexneighbors()
     end
